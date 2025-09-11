876c7910d12df61f32c71aca986e5a18a1f284f0
Pratiksha914 <pratikshapatidar20@gmail.com>
2025-09-11 08:32:36 +0530

    Update CipherBox.cpp

--- CipherBox.cpp
+++ CipherBox.cpp
@@ -3,3 +3,35 @@
 #include <limits>
 
 using namespace std;
+
+class Helper
+{ 
+public:
+bool isAlpha(char c) {
+  return (c>='A' && c<='Z') || (c>='a' && c<='z');
+}
+bool isUpper(char c) {
+  return (c>='A' && c<='z');
+}
+char toUpper (char c)
+{
+  if (c>='a' && c<='z')
+    return c-'a'+'A';
+return c;
+}
+char toLower(char c)
+{
+  if (c>='A' && c<='Z')
+    return c-'A'+'a';
+return c;
+}
+    
+
+
+
+
+
+
+
+
+
