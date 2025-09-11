5c00b64d7c341a2fe9018f811dc23deec0a03b44
PankhuriK415 <pankhurik415@gmail.com>
2025-09-11 08:34:48 +0530

    Update CipherBox.cpp

--- CipherBox.cpp
+++ CipherBox.cpp
@@ -3,3 +3,57 @@
 #include <limits>
 
 using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void reversestrng (string &s)
+{
+  int n = s.length() ;
+  for (int i = 0; i < n/2; i++)
+  {
+     char temp = s[i];
+s[i] = s[n - i - 1];
+s[n - i - 1] = temp;
+  }
+}
+
+int getinput (string prompt, int minval, int maxval)
+{
+  int choice;
+while (true)
+{
+cout << prompt;
+if (cin >> choice && choice >= minval && choice <= maxval)
+{
+  return choice;
+}
+else
+{
+cin.clear ();
+string badInput;
+cin >> badInput;
+cout<< " Invalid Input!! Enter a number between" << minval << " and" << maxval << "\n";
+}
+}
+}
+
