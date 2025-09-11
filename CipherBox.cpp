fee9f15c870c0b3e46f9d4969a4c3afd4b383a64
PankhuriK415 <pankhurik415@gmail.com>
2025-09-11 09:34:32 +0530

    Update CipherBox.cpp

--- CipherBox.cpp
+++ CipherBox.cpp
@@ -25,3 +25,32 @@ char toLower(char c)
     return c-'A'+'a';
 return c;
 }
+void reverseString (string &s)
+{
+  int n = s.length();
+for (int i = 0; i < n/2; i++)
+{
+char temp = s[i];
+s[i] = s[n-i-1];
+s[n-i-1] =temp;
+}
+}
+int getInput(string prompt, int minval. int maxval)
+{
+  int choice;
+while (true)
+{
+cout << prompt;
+if (cin >> choice && choice >= minval && choice <= maxval)
+{
+return choice;
+}
+else
+{
+cin.clear();
+string badInput;
+cin >> badInput;
+cout << "Invalid Input!! Enter a number between " << minval << "and" << maxval << "\n";
+}
+}
+}
